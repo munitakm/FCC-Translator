c9b6b213f12b568d00d52116c8e61742db753865
renan <renan.manshio@gmail.com>
2021-11-05 20:53:50 -0300

    working on the logic of translator.js

--- components/translator.js
+++ components/translator.js
@@ -1,9 +1,20 @@
-const americanOnly = require('./american-only.js');
-const americanToBritishSpelling = require('./american-to-british-spelling.js');
-const americanToBritishTitles = require("./american-to-british-titles.js")
-const britishOnly = require('./british-only.js')
+const aOnly = require('./american-only.js');
+const aToBSpelling = require('./american-to-british-spelling.js');
+const aToBTitles = require("./american-to-british-titles.js")
+const bOnly = require('./british-only.js')
 
 class Translator {
+
+britishAmerican (string) { 
+const lString = string.toLowerCase();
+let words = Object.keys(bOnly).filter(i => lString.includes(i))
+  for(let c of words) { 
+  var r = c;
+  var re = new RegExp(r,'gi');
+    string = string.replace(re, bOnly[c])
+  }
+  return string;
+}
 
 }
 
--- routes/api.js
+++ routes/api.js
@@ -8,6 +8,13 @@ module.exports = function (app) {
 
   app.route('/api/translate')
     .post((req, res) => {
-      
+     const form = req.body;
+      console.log(form)
+      if(!form.locale || !form.text) res.json({error: 'Required field(s) missing'}) 
+      if(form.text == '') return res.json({error: 'No text to translate'});
+      if(form.locale !== 'british-to-american' && 
+         form.locale !== 'american-to-british')
+        return res.json({error: 'Invalid value for locale field'});
+      res.json({translation: form.text})
     });
 };
